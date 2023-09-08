8b9691b672c668456fc100e11fa28aade5a6ac19
IshtamarFr <ishta@ishtamar.fr>
2023-09-08 15:34:17 +0200

    createChart working (with placeholders)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,9 +8,14 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, PieChartComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PieChartComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  styleUrls: ['./pie-chart.component.scss'],
 })
 export class PieChartComponent implements OnInit {
+  public chart: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.createChart();
   }
 
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'pie',
+
+      data: {
+        // values on X-Axis
+        labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [300, 240, 100, 432, 253, 34],
+            backgroundColor: [
+              'red',
+              'pink',
+              'green',
+              'yellow',
+              'orange',
+              'blue',
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
 }
