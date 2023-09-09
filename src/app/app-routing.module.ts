fa6afe23f3fb5afb33c3e0fad6b6f93eda4804c0
IshtamarFr <ishta@ishtamar.fr>
2023-09-09 15:52:18 +0200

    Generated country component (no lazy routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CountryComponent } from './pages/country/country.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: ':id',
+    component: CountryComponent,
+    /*I dont use any lazy-loading as the child path has only one component
+    It could be good to use that if design gets weirder (with modules at same time)*/
   },
   {
     path: '**', // wildcard
